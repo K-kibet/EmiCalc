214dfa44d42333f5cd8842397dee7705923a0f0c
K-kibet <kkibetkkoir@gmail.com>
2025-07-02 18:11:59 +0300

    updated commit

--- app/src/main/java/com/kernelapps/emicalc/ads/AdsConstant.kt
+++ app/src/main/java/com/kernelapps/emicalc/ads/AdsConstant.kt
@@ -3,10 +3,10 @@ package com.kernelapps.emicalc.ads
 object AdsConstant {
 
 
-    const val admobAppOpenId: String = "ca-app-pub-3940256099942544/9257395921"
-    const val admobNativeId: String="ca-app-pub-3940256099942544/2247696110"
-    const val admobInterstitialId: String="ca-app-pub-3940256099942544/1033173712"
-    const val admobBannerId: String="ca-app-pub-3940256099942544/9214589741"
+    const val admobAppOpenId: String = "ca-app-pub-8271781915615155/8295400712"
+    const val admobNativeId: String="ca-app-pub-8271781915615155/6916554646"
+    const val admobInterstitialId: String="ca-app-pub-8271781915615155/8441389300"
+    const val admobBannerId: String="ca-app-pub-8271781915615155/3380634310"
     const val displayCounter=1
 
 
